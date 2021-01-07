73bb955953beb5c4700850101de36d1bafefbe71
Andrey Mironov <andrey@MacBook-Pro-Andrey.local>
2021-01-07 19:28:40 +0300

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,24 +7,20 @@ import user from '../../img/user.svg';
 const Header = () => {
   return (
     <div className="header">
-      <head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,700;1,300&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
-      </head>
       <div className="section">
         <div className="header">
           <div className="header_logo">
-            <a href="">
-              <img className="logo" src={logoHeader} alt=""/>
+            <a href="/">
+              <img className="logo" src={logoHeader} alt="" />
             </a>
           </div>
           <nav className="header_nav">
-            <a className="header_link" href="">
+            <a className="header_link" href="/">
               <button className="link_btn">Войти</button>
             </a>
-            <a className="header_btn" href="">
-              <button>
-                <img className="btn_sign_in" src={user} alt=""/>
+            <a className="header_btn" href="/">
+              <button className="btn">
+                <img className="btn_sign_in" src={user} alt="" />
               </button>
             </a>
           </nav>
